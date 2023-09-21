06e225c57f5e12d9f55a07984fa43e990df47f68
SawdaDevs <sawda.onanuga@gmail.com>
2023-09-22 00:31:28 +0100

    first commit

---
+++ dateWeekend.js
@@ -0,0 +1,43 @@
+function run(birthday_date) {
+	/*
+	* Write your code below; return type and arguments should be according to the problem\'s requirements
+	*/
+	let future_dates = ""
+	let newBirthDate = birthday_date.split("-")
+	newBirthDate[0]= parseInt(newBirthDate[0])
+    newBirthDate[1]= parseInt(newBirthDate[1])
+	let dayOfWeek =
+	{
+		0: 'Sun',
+		1: 'Mon',
+		2: 'Tues',
+		3: 'Wed',
+		4: 'Thu',
+		5: 'Fri',
+		6: 'Sat'
+	}
+
+	for ( let year=2016; year<2066; year++ ){
+		let currDate = new Date(year,(newBirthDate[1]-1),newBirthDate[0])
+
+		if (currDate.getDay() ===0 || currDate.getDay()===5 || currDate.getDay()===6)
+    	{
+			console.log("month",currDate.getMonth(), 'day', currDate.getDay())
+            if((new Date(year, currDate.getMonth(), currDate.getDate()) === 1) ){
+                if(year%4===0){
+                    console.log(year)
+                    future_dates+= `${dayOfWeek[currDate.getDay()]}-${year} `
+                }
+                }
+            else{
+                future_dates+= `${dayOfWeek[currDate.getDay()]}-${year} `
+            }
+
+			
+		}
+	}
+	
+	return future_dates;
+}
+
+console.log(run("29-02"))
